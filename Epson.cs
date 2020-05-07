89b768f8cfde420a88c8cf5fdb071ea60d403357
M Hatimi Al Jaudi <61742580+mhatimi7@users.noreply.github.com>
2020-05-07 21:58:37 +0700

    Tugas Lab 5

---
+++ Epson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorp
+{
+    class Epson : PrinterWindows
+    {
+        public override void show()
+        {
+            Console.WriteLine("\nEpson display dimension : 10*11");
+        }
+        public override void print()
+        {
+            Console.WriteLine("Epson printer printing. . .");
+        }
+    }
+}
