755b76b9dadfda2b9fac0864805ef946d582fc5a
M Hatimi Al Jaudi <61742580+mhatimi7@users.noreply.github.com>
2020-05-07 22:01:18 +0700

    Tugas Lab 5

---
+++ PrinterWindows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorp
+{
+    class PrinterWindows
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("\nPrinter display dimension");
+        }
+        public virtual void print()
+        {
+            Console.WriteLine("Printer printing");
+        }
+    }
+}
